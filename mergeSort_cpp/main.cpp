c7fca939fbbd06ad45767b53a381ea36fce7561d
asb-school <asb@andrews.edu>
2012-12-05 20:39:11 -0500

    Initial Commit

---
+++ mergeSort_cpp/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  mergeSort_cpp
+//
+//  Created by Andrew Breja on 12/5/12.
+//  Copyright (c) 2012 Andrew Breja. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
